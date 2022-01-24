f0d995d47fad5c02d36e89ff7d59257b20785ce8
matias davila <mipcomputacion@gmail.com>
2022-01-23 23:03:06 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cursoreact',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
